d51fe7574e8091fcd005069f5a0b84553b7dbd96
SOOUHA <92102868+SOOUHA@users.noreply.github.com>
2023-11-11 12:55:01 +0100

    Add test

--- src/test/java/tn/esprit/spring/services/TestActivitySector.java
+++ src/test/java/tn/esprit/spring/services/TestActivitySector.java
@@ -8,8 +8,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tn.esprit.devops_project.entities.ActivitySector;
 import tn.esprit.devops_project.repositories.ActivitySectorRepository;
 import tn.esprit.devops_project.services.ActivitySectorImpl;
