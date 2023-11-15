4dcf8a78e0e13b2fcebfeca37a27f2ac1b91f8f0
0x7ac4 <93057879+ahmed-hachani@users.noreply.github.com>
2023-11-15 20:49:34 +0100

    Added front angular code source inside project root

---
+++ DevOps_Project_Front/documentation/js/compodoc.js
@@ -0,0 +1,14 @@
+let compodoc = {
+    EVENTS: {
+        READY: 'compodoc.ready',
+        SEARCH_READY: 'compodoc.search.ready'
+    }
+};
+
+Object.assign( compodoc, EventDispatcher.prototype );
+
+document.addEventListener('DOMContentLoaded', function() {
+    compodoc.dispatchEvent({
+        type: compodoc.EVENTS.READY
+    });
+});
